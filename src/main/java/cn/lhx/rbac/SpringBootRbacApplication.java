06ea7108f6fb7b4bb7488130a180d44d65cfda62
leee549 <1193106371@qq.com>
2020-03-25 11:20:01 +0800

    创建测试

---
+++ src/main/java/cn/lhx/rbac/SpringBootRbacApplication.java
@@ -0,0 +1,15 @@
+package cn.lhx.rbac;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("cn.lhx.rbac.dao")
+public class SpringBootRbacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRbacApplication.class, args);
+    }
+
+}
